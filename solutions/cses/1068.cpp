abe31c38c87993d4c1ea464b212b56337f476c93
최광윤 <shinhwa30724@gmail.com>
2026-09-15 01:39:33 +0900

    Solved Day 1 / CSES 1068, 1083
    Solved Day 1 / CSES 1068, 1083

---
+++ solutions/cses/1068.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstdint>
+
+void Task1068() {
+    // Task : https://cses.fi/problemset/task/1068/
+    uint64_t element = 0;
+    std::cin >> element;
+    
+    while (true)
+    {
+        std::cout << element << " ";
+        if      (element == 1)   break;
+        else if (element % 2 == 0)  element /= 2;
+        else    element = element * 3 + 1;
+    }
+}
+
+int main(){
+    Task1068();
+    return 0;
+}
---
+++ solutions/cses/1083.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstdint>
+#include <vector>
+
+void Task1083() {
+    // Task : https://cses.fi/problemset/task/1083/
+    uint32_t MaxNumber = 0;
+    std::cin >> MaxNumber;
+    
+    std::vector<uint32_t> NumberList;
+    NumberList.resize(MaxNumber+1, 1);
+    uint32_t Numb = 0;
+    while (std::cin>>Numb)
+    {
+        NumberList[Numb] = 0;
+    }
+    
+    for (uint32_t i = 1; i <= MaxNumber; ++i)
+    {
+        if (NumberList[i] != 0)
+        {
+            std::cout << i << " ";
+        }
+    }
+}
+
+int main(){
+    Task1083();
+    return 0;
+}
